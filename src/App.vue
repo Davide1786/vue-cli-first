9220e2dc039561248ed200ad2bb0a2982a29ae2e
Davide <dadavidenapolitano@gmail.com>
2021-09-17 16:47:37 +0200

    Esercizio completato

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld /> -->
+    <Galleria />
+    <div>
+      <hamsik />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Galleria from './components/Galleria.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Galleria
+
   }
 }
 </script>
